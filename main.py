c41b14557be9cafa616f0c77d5c477a6ac6df670
Emmanuel  Omondi <94011148+Omondi85Manu@users.noreply.github.com>
2022-12-16 21:59:09 +0300

    Added Hello emoji

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 
 def print_hello_world_twice():
-    print("Hello World 1")
-    print("Hello World 2")
+    print("👋Hello World 1")
+    print("👋Hello World 2")
 
 
 def print_hello_world_multiple_times(times):
     for i in range(1, times+1):
-        print("Hello World")
+        print("👋Hello World!")
 
 
 # print("Hello World 3")
